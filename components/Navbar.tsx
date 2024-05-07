db8003b4d3bbd2423f4fbc56270047e8f206f728
kindesskay <kindenessudubamo@gmail.com>
2024-05-08 00:47:19 +0100

    import module problem

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,9 @@
-"use client";
 import Image from "next/image";
-import { usePathname, useRouter } from "next/navigation";
 export default function NavBar() {
-  const router = useRouter();
-  const path = usePathname();
-
   return (
     <header className=" w-full fixed z-10 max-w-lg">
       <nav className="h-12 w-full flex items-center mt-4">
-        <button
-          className="h-10 w-10 "
-          onClick={() => router.back()}
-          hidden={path == "/"}
-        >
+        <button className="h-10 w-10 ">
           <Image
             src={"/assets/back.png"}
             alt="back-icon"
