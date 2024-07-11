9320ca5a1f4c245f0ff62df2e93df068a527364f
kindesskay <kindenessudubamo@gmail.com>
2024-07-11 11:58:19 +0100

    font size increase

--- app/(root)/app/page.tsx
+++ app/(root)/app/page.tsx
@@ -4,7 +4,7 @@ import DateDisplay from "@/components/DateDisplay";
 
 export default function page() {
   return (
-    <section className="w-full flex flex-col items-center gap-2 min-h-screen">
+    <section className="w-full flex flex-col items-center gap-2 ">
       <>
         <DateDisplay />
         <HoriazontalSlider />
--- components/TaskView.tsx
+++ components/TaskView.tsx
@@ -15,16 +15,16 @@ export default function TaskView({ task, handleClose, deleteItem }: task) {
   };
   let new_date = new Date(date).toDateString();
   return (
-    <div className="rounded-lg bg-white overflow-hidden p-2 flex flex-col  w-full  h-full relative shadow-md">
+    <div className="rounded-lg  overflow-hidden p-2 flex flex-col  w-full  h-full relative">
       <div className="flex justify-between">
         <button
           onClick={handleClose}
-          className="rounded-full z-10 h-8 w-8 bg-slate-100 text-white p-2 "
+          className="rounded-full z-10 h-12 w-12 hover:shadow-sm p-2 "
         >
           <img src="/assets/close.png" alt="close" />
         </button>
         <button
-          className="w-8 h-8 rounded-full overflow-hidden"
+          className="w-12 h-12 rounded-full overflow-hidden"
           onClick={deleteItem}
         >
           <Image
@@ -37,13 +37,11 @@ export default function TaskView({ task, handleClose, deleteItem }: task) {
         </button>
       </div>
 
-      <div className="w-full relative flex flex-col px-2 ">
-        <h2 className="text-xl font-semibold text-gray-500">{title}</h2>
-        <p className="text-xs font-light text-gray-600 font-mono ">
-          Transaction on {new_date}
-        </p>
+      <div className="w-full relative flex flex-col p-4 ">
+        <h2 className="text-2xl font-semibold text-gray-500">{title}</h2>
+        <p className="font-light text-gray-600 font-mono ">{new_date}</p>
 
-        <p className="text-sm ">{note}</p>
+        <p className="text-lg ">{note}</p>
       </div>
     </div>
   );
